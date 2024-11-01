c85fdda1c7221df9a0820a9ed5079433358565da
Nicholas Mendez <snickdx@gmail.com>
2024-11-01 12:35:46 -0400

    first commit

---
+++ src/data/siteMeta.ts
@@ -0,0 +1,25 @@
+/* -----------------------------------
+ *
+ * Meta
+ *
+ * -------------------------------- */
+
+const pageTitle = '11tyby';
+
+/* -----------------------------------
+ *
+ * Meta
+ *
+ * -------------------------------- */
+
+const metaDescription = `
+  Simple 11ty setup using TypeScript, SASS, Preact with partial hydration, and other useful things. Aims to provide the DX of Gatsby, but using 11ty!
+`;
+
+/* -----------------------------------
+ *
+ * Export
+ *
+ * -------------------------------- */
+
+export { pageTitle, metaDescription };
---
+++ src/modules/home/components/form/form.component.tsx
@@ -0,0 +1,59 @@
+import { h, Fragment } from 'preact';
+import { useState } from 'preact/hooks';
+import style from './form.module.scss';
+
+/* -----------------------------------
+ *
+ * IProps
+ *
+ * -------------------------------- */
+
+interface IProps {
+  title: string;
+  className?: string;
+}
+
+/* -----------------------------------
+ *
+ * Form
+ *
+ * -------------------------------- */
+
+function Form({ title, className = '' }: IProps) {
+  const [value, setValue] = useState('');
+
+  return (
+    <Fragment>
+      <h2 class={style.title}>{title}</h2>
+      <label htmlFor="demo" class={style.label}>
+        Type something:
+      </label>
+      <input
+        id="demo"
+        type="text"
+        onKeyUp={onKeyUp}
+        className={style.input}
+        placeholder="..."
+      />
+      {value && (
+        <p class={style.text}>
+          <em>Value:</em> {value}
+        </p>
+      )}
+    </Fragment>
+  );
+
+  function onKeyUp(event: Event) {
+    const { value } = event.target as HTMLInputElement;
+
+    setValue(value);
+  }
+}
+
+/* -----------------------------------
+ *
+ * Export
+ *
+ * -------------------------------- */
+
+export { Form };
